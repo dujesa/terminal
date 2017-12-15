2f8f95e80d619873053b6a93a834cae1aafe1944
dujesaric <33405160+dujesaric@users.noreply.github.com>
2017-12-16 00:22:19 +0100

    dodan slider

--- includes/head.php
+++ includes/head.php
@@ -12,12 +12,17 @@
 		<title><?php echo $ptitle . "Početna";?></title>
 		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
 		<link rel="icon" href="/favicon.ico" type="image/x-icon">
+	
+		
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+		<!-- jQuery library -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<!-- Popper JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+		<!-- Latest compiled JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
 		<link rel="stylesheet" type="text/css" href ="css/stil.css">
-		<!--includan jQuery ako bude treba-->
-		<script src="https://code.jquery.com/jquery-3.2.1.slim.js"
-  				integrity="sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg="
-  				crossorigin="anonymous">
-  		</script>
 	</head>
 
 	<body>
--- includes/slider.php
+++ includes/slider.php
@@ -1,1 +1,40 @@
-<div class="slider">Slider</div>
+<div class="slider">
+
+	<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/sliderImages/sliderImg1.png" alt="Los Angeles" width="100%" height="500">
+      <div class="carousel-caption">
+        <h3>Pouzdanost, sigurnost, dostupnost...</h3>
+        <p></p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/sliderImages/sliderImg2.jpg" alt="Chicago" width="100%" height="500">
+      <div class="carousel-caption">
+        <h3></h3>
+        <p></p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/sliderImages/sliderImg3.png" alt="New York" width="100%" height="500">
+      <div class="carousel-caption">
+        <h3>We are your home</h3>
+        <p></p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+</div>
